6d48d230c0fcd5463961466769708f8bd8fbcf6b
khalifeyifatemeh <khalifeyifatemeh@gmail.com>
2016-03-04 09:35:27 -0800

    start project add comment and parentheses
    add comment and parentheses

--- Homework2_datamining.cpp
+++ Homework2_datamining.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include<conio.h>
 #include<iostream>
 #include<string>
@@ -19,7 +19,7 @@ int main()
 	string b = "";
 	vector <double> vv;
 	int j;
-	for ( j = 0; j < s.length(); j++)
+	for ( j = 0; j < s.length(); j++)                 //جداسازی اعداد ورودی و تبدیل از حالت رشته به عدد
 	{
 		if (s[j] != ',')
 		{
@@ -27,43 +27,27 @@ int main()
 		}
 		else
 		{
-
-			double a = 0;
-			/*for (int h = 0; h <b.length(); h++)
-			{
-				a *= 10;
-				a += (b[h] - 48);
-				
-			}*/
 			vv.push_back(stod(b,NULL));
 			b = "";
 		}
 	}
-	/*double a = 0;
-	for (int h = 0; h <b.length(); h++)
-	{
-		a *= 10;
-		a += (b[h] - 48);
-
-	}*/
-	vv.push_back(stod(b,NULL));
-	sort(vv.begin(),vv.end());
-
+	vv.push_back(stod(b,NULL));                       // برای آخرین عدد-> تبدیل از حالت رشته به عدد
+	sort(vv.begin(),vv.end());                        //مرتب سازی لیست اعداد
 	double min;
 	double max;
 	double Q1;
 	double Q2;
 	double Q3;
-
 	cout << "for this data:" << endl;
-	cout << "MIN:" << vv[0] << endl;
-	if (vv.size() % 2 == 0)
+	cout << "MIN:" << vv[0] << endl;        //چاپ مقدار مینیمم
+
+	if (vv.size() % 2 == 0)                 //محاسبه ی مقادیر Q1,Q2,Q3
 	{
 		Q2 = (vv[(vv.size() / 2)-1] + vv[(vv.size() / 2)]) / 2;
 		double n = vv.size() / 2;
 		if ((vv.size() / 2) % 2 == 0)
 		{
-			Q1 = (vv[(n / 2)-1] + vv[n / 2])/2;
+			Q1 = ((vv[(n / 2)-1]) + (vv[n / 2]))/2;
 			Q3 = (vv[n + (n / 2) - 1] + vv[n + (n / 2)])/2;
 		}
 		else
@@ -78,18 +62,17 @@ int main()
 		double m = vv.size() / 2;
 		if ((vv.size() / 2) % 2 == 0)
 		{
-			Q1 = (vv[(m / 2) - 1] + vv[(m / 2)]) / 2;
-			Q3 = (vv[m + (m / 2) - 1] + (vv[m + (m / 2)]) / 2);
+			Q1 = ((vv[(m / 2) - 1]) + (vv[(m / 2)]) )/ 2;
+			Q3 =( (vv[m + (m / 2)]) + (vv[m+1 + (m / 2)]))/2;
 		}
 		else
 		{
 			Q1 = vv[m / 2];
 			Q3 = vv[m +1+ (m / 2)];
 		}
-
 	}
-	max = vv[vv.size() - 1];
-	cout.setf(ios::fixed);
+	max = vv[vv.size() - 1];                            //چاپ مقدار ماکسیمم
+	cout.setf(ios::fixed);                             // جلوگیری از نمایش عدد با نماد علمی
 	cout << "Q1:" << setprecision(2) << Q1 << "--" << "Q2:" << setprecision(2) << Q2 << "--" << "Q3:" << setprecision(2) << Q3 << endl;
 	cout << "MAX:" << max<<endl;
 
@@ -98,7 +81,7 @@ int main()
 	double o = nn + Q3;  //x>o
 	double oo = Q1 - nn; //x<oo
 	vector<double> vvv;
-	for (int f = 0; f < vv.size(); f++)
+	for (int f = 0; f < vv.size(); f++)                   //بررسی وجود و عدم وجود outlier
 	{
 		if (vv[f]>o)
 			vvv.push_back(vv[f]);
